20427c98d3fe3ae7475a5a10ad4a3a43bd132176
Dinh Huy <huydinhse@gmail.com>
2022-12-09 07:29:15 +0700

    create project

---
+++ 19508631_DinhQuangHuy/src/main/java/com/example/dinhquanghuy19508631/DinhQuangHuy19508631Application.java
@@ -0,0 +1,13 @@
+package com.example.dinhquanghuy19508631;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinhQuangHuy19508631Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinhQuangHuy19508631Application.class, args);
+    }
+
+}
---
+++ 19508631_DinhQuangHuy/src/test/java/com/example/dinhquanghuy19508631/DinhQuangHuy19508631ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dinhquanghuy19508631;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinhQuangHuy19508631ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
